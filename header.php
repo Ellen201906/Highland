819f39e821b34af49bd84d5e4d4106a0539bb9f9
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2019-06-19 15:46:29 -0600

    Add files via upload

--- header.php
+++ header.php
@@ -14,16 +14,16 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
- <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdn.staticfile.org/popper.js/1.12.5/umd/popper.min.js"></script>
-  <script src="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdn.staticfile.org/popper.js/1.12.5/umd/popper.min.js"></script>
+<script src="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/js/bootstrap.min.js"></script>
 
   
 
 <style>
  #header1{
- margin-left:20px;
- font-size:26px;
+ margin-left:10px;
+ font-size:20px;
  }
  nav{
  font-size:20px;
@@ -33,19 +33,7 @@
 <div> 
     <div id="header1">
 	 <script>
-	 $(window).scroll(function() {
-            //小屏幕下的导航条折叠
-            if($(window).width() < 768) {
-                //点击导航链接之后，把导航选项折叠起来
-                $("#navbar a").click(function() {
-                    $("#navbar").collapse('hide');
-                });
-                //滚动屏幕时，把导航选项折叠起来
-                $(window).scroll(function() {
-                    $("#navbar").collapse('hide');
-                });
-            }
-        });
+
 
             var d = new Date();
             var curTime=d.getHours();
@@ -64,7 +52,7 @@
 	</div>
  
   <nav class="navbar navbar-expand-lg navbar-light bg-success" id="navbar">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
