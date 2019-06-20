0a0aa62d8c93b852b082d5c6cebcfd9ac80b4c52
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2019-06-19 23:36:32 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -49,17 +49,7 @@
     <?php include 'header.php';?>
 
  
-  <!-- bing map-->
-  <div style="text-decoration:none; overflow:hidden;max-width:100%;width:380px;height:400px;float:right">
-  <div id="googlemaps-display" style="height:100%; width:100%;max-width:100%;">
-  <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=8204+jasper+ave+edmonton+canada&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
-  </iframe>
-  </div>
-  <a class="embedmap-code" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="authmap-data">premium bootstrap themes
-  </a>
-  <style>#googlemaps-display img.text-marker{max-width:none!important;background:none!important;}img{max-width:none}</style>
-  </div>
-
+  
   <div >
     <img src="store.jpg" class="d-block imgs" alt="UPS"  >
 
@@ -78,7 +68,22 @@
   <p><i class="fas fa-phone-volume"></i> Phone number: 780-328-3069</p>
 
   <p><i class="fas fa-compass"></i> Address: 8204 Jasper Ave NW, Edmonton AB T5H 3S2</p>
+
+   <a class="nav-link " href="#" id="navbardrop" data-toggle="dropdown" data-hover="dropdown" title="click to open or close map" style="margin-left:-15px;color:black"><i class="fas fa-globe"></i>Click to see map</a>
+      
+  <div class="dropdown-menu">
+          		    <!-- bing map-->
+  <div style="text-decoration:none; overflow:hidden;max-width:100%;width:880px;height:400px;margin-left:200px">
+  <div id="googlemaps-display" style="height:100%; width:100%;max-width:100%;">
+  <iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=8204+jasper+ave+edmonton+canada&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
+  </iframe>
   </div>
+  <a class="embedmap-code" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="authmap-data">premium bootstrap themes
+  </a>
+  <style>#googlemaps-display img.text-marker{max-width:none!important;background:none!important;}img{max-width:none}</style>
+  </div>
+           
+       </div>
   
   <br>
   <div class="container" style="margin-left:5px">
@@ -98,7 +103,7 @@
         <div class="col-md-8 mb-3">
             <label for="validationDefaultEmail">Email (required)</label>
             <div class="input-group">
-            <input type="text" class="form-control" id="validationDefaultEmail" placeholder="Email" name="email" required>
+            <input type="email" class="form-control" id="validationDefaultEmail" placeholder="Email" name="email" required>
             </div>
         </div>
     </div>
@@ -113,5 +118,6 @@
 
     <!-- footer -->
     <?php include 'footer.php';?>
+	
 </body>
 </html>
