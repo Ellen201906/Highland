86851720fda29a361657db56bab5c619d77e273f
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2019-06-20 10:35:36 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 	 <img src="webwxgetmsgimg.jpg" class="d-block imgs" alt="store" >
 
    </div>
-   <br><br><br>
+   <br><br><br><br><br>
   <div id="body1">
   <p><i class="far fa-clock"></i>Business hours:   Monday to Sunday  |   9:00am to 9:00pm</p>
 
