a4d1e0311dec986eb896715327bdd476d6f9de93
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2020-01-12 11:18:34 -0700

    Update body.php

--- body.php
+++ body.php
@@ -2,7 +2,7 @@
  <html>
 <body>
   
-  <div class="container" id="body1" style="font-size:20px;margin-top:-15px:margin-left:30px;color:black">
+  <div class="container" id="body1" style="color:white">
   <p><i class="far fa-clock"></i>Business hours:   Monday to Sunday  |   9:00am to 9:00pm</p>
 
   <p><i class="fas fa-phone-volume"></i> Phone number: 780-328-3069</p>
@@ -10,7 +10,7 @@
   <p style="margin-top:16px"><i class="fas fa-compass"></i> Address: 8204 Jasper Ave NW, Edmonton AB T5H 3S2</p>
   
   <i class="fas fa-envelope-square"></i><a href="Mailto:liuye5@hotmail.com?&Subject=From Highland's customer" style=" text-decoration: underline;;color:black;margin-left:5px">Email to me</a>
-  <a class="nav-link " href="#" id="navbardrop" data-toggle="dropdown" data-hover="dropdown" title="click to open or close map" style=" text-decoration: underline;color:black;margin-left:-10px;margin-top:10px"><i class="fas fa-globe"></i>Click to see map</a>   
+  <p><a class="nav-link " href="#" id="navbardrop" data-toggle="dropdown" data-hover="dropdown" title="click to open or close map" style=" text-decoration: underline;color:black;margin-left:-10px;margin-top:10px"><i class="fas fa-globe"></i>Click to see map</a><p>   
   <div class="dropdown-menu">
   <!-- bing map-->
   <div style="text-decoration:none; overflow:hidden;max-width:100%;width:480px;height:400px;">
